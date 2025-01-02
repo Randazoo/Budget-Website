c2b380f9abb3cfb9882c4e6ae1ce0afe589d2b7d
Randazoo <dtaube02@gmail.com>
2025-01-02 17:03:31 -0500

    Delete pages directory

--- pages/_app.js
+++
@@ -1,7 +0,0 @@
-import '../../lib/db'; // Correct relative path from pages/_app.js to lib/db.js
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp;
